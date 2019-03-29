a936cf233b99bacb0a55f13cb9d4b9ed817c725c
Erich Alves Fratini <erich.fratini.1998@gmail.com>
2019-03-29 10:42:26 -0300

    s

--- src/test/java/com/framework/support/Web.java
+++ src/test/java/com/framework/support/Web.java
@@ -1,5 +1,6 @@
 package com.framework.support;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,13 +11,14 @@ public class Web {
     public static final String BINARY_CHROME_DRIVER = "src/main/resources/drivers/chromedriver";
 
     public static WebDriver getChromeDriver() {
-
+        Dimension d = new Dimension(1382,744);
         ChromeOptions options = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", BINARY_CHROME_DRIVER);
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         WebDriver navegador = new ChromeDriver(options);
-        navegador.manage().window().maximize();
-        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //navegador.manage().window().maximize();
+        navegador.manage().window().setSize (d);
+        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return navegador;
 
     }
